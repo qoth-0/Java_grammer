626f8d7751a55707c9cd52361583da7605321a0a
qoth-0 <bny1324@naver.com>
2023-12-27 09:15:09 +0900

    .

--- src/C17ExceptionFileParsing/AuthorException/AuthorService.java
+++ src/C17ExceptionFileParsing/AuthorException/AuthorService.java
@@ -11,7 +11,7 @@ class AuthorService { // 비즈니스 로직 처리
     }
     void register(Author author) throws IllegalArgumentException{
 //        만약에 password가 5자리 이하이면 예외 발생(IllegalArgument)
-//        기존에 같은 email있는 지도 체크
+//        기존에 같은 email이 있는지도 체크
         if(author.getPassword().length() < 5) {
             throw new IllegalArgumentException("비밀번호가 너무 짧습니다.");
         }
@@ -25,7 +25,7 @@ class AuthorService { // 비즈니스 로직 처리
     }
     Optional<Author> login(String email, String password) throws IllegalArgumentException, NoSuchElementException { // login했을 때 사용자가 있을수도 없을수도 있기 때문에 optional 사용
         Optional<Author> author = authorRepository.getAuthorByEmail(email);
-//        email이 존재하지 않으면 예외발생(NoSuchElement) - for문 돌ㄹ려서 찾아야됨
+//        email이 존재하지 않으면 예외발생(NoSuchElement)
 //        password가 틀리면 예외발생(IllegalArgument)
         if(author.isPresent()) {
             if(author.get().getPassword().equals(password)){
